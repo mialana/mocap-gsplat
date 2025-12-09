792d8043e57612d98b9ba9ff83c3e27da54e3fd1
Amy Liu <mialana@users.noreply.github.com>
2025-12-09 12:22:02 -0500

    feat: refined preprocessing scripts

--- Makefile
+++ Makefile
@@ -1,19 +1,7 @@
 .PHONY: all
 
-clone:
-	git clone --recurse-submodules repo_url
+process_frames:
+	python scripts/avi_to_frames.py resources/raw_data/caroline_shot --start_frame=0 --end_frame=24
 
-viser:
-	python src/vggt/demo_viser.py --image_folder resources/colmap_project_dir/images
-
-gradio:
-	python src/vggt/demo_gradio.py
-
-colmap:
-	python external/vggt/demo_colmap.py --scene_dir=resources/colmap_project_dir_synthetic --use_ba --max_query_pts=1024 --query_frame_num=8
-
-extra-installs:
-	pip install --no-build-isolation git+https://github.com/rahul-goel/fused-ssim@328dc9836f513d00c4b5bc38fe30478b4435cbb5 git+https://github.com/harry7557558/fused-bilagrid@90f9788e57d3545e3a033c1038bb9986549632fe
-
-train:
-	python src/gsplat/simple_trainer.py default --disable_video --data_factor 1 --data_dir colmap_project_dir --result_dir RESULTS --save_ply
+rotate_frames:
+	python scripts/preprocess.py --source_dir=resources/raw_data/caroline_shot/FRAMES --start_frame=0 --end_frame=24
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -10,21 +10,30 @@ def rotate_imgs(image_dir):
     for i in range(3,8):
         img_pth = img_paths[i]
 
-        print(f"Processing image at {img_pth}")
-
         img = Image.open(img_pth)
         img = img.transpose(Image.ROTATE_180)
+
+        # print(f"Processing image at {img_pth}")
 
         img.save(img_pth)
 
 def main():
     parser = argparse.ArgumentParser(description="Process two directories of background/source images in batch")
-    parser.add_argument("--background_dir", type=str, help="directory of background images", default=r"resources/raw_data/empty_shot/FRAMES/frame0000")
-    parser.add_argument("--source_dir", type=str, help="directory of source images", default=r"resources/raw_data/caroline_shot/FRAMES/frame0000")
+    parser.add_argument("--background_dir", type=str, help="directory of background images", default=r"resources/raw_data/empty_shot/FRAMES/")
+    parser.add_argument("--source_dir", type=str, help="directory of source images", default=r"resources/raw_data/caroline_shot/FRAMES/")
+    parser.add_argument("--start_frame", type=int, default=0, help="Start frame number (inclusive).")
+    parser.add_argument("--end_frame", type=int, default=24, help="End frame number (exclusive).")
+
     args = parser.parse_args()
 
-    rotate_imgs(args.background_dir)
-    rotate_imgs(args.source_dir)
+    for frame_idx in range(args.start_frame, args.end_frame):
+        source_dir_path = os.path.join(args.source_dir, f"frame{frame_idx:04d}")
+        rotate_imgs(source_dir_path)
+        print(f"Processing frame {frame_idx} complete.")
+
+
+
+    # rotate_imgs(args.background_dir)
 
 if __name__ == '__main__':
     main()
