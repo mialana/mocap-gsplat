52bd9ed28d5e01faa7f86f097ae2d5a67e572ac0
mialana <aliu@amyliu.dev>
2026-01-12 09:39:05 -0500

    feat: develop a pipeline for github releases

--- scripts/build.py
+++ scripts/build.py
@@ -4,16 +4,19 @@ Author : Amy Liu <aliu@amyliu.dev>
 Purpose:
 1. Download PyPI wheels
 2. Generate `blender_manifest.toml`
-3. Generate `.env`
+3. Package addon as ZIP file
+4. Clean PyPI wheels directory (optional)
+5. Install PyPI wheels to local Python interpreter for development (optional)
 """
 
 import argparse
-from pathlib import Path
 import sys
 import os
 import shutil
 import subprocess
 import datetime
+import zipfile
+from pathlib import Path
 from typing import Tuple
 from dataclasses import dataclass, fields
 
@@ -23,9 +26,11 @@ class BuildContext:
     """capitalized indicates it is an input resource (i.e. expected to exist)"""
 
     timestamp_str: str
+    version_tag: str
     wheels_dir: Path
     ADDON_SRC_DIR: Path
-    REQUIREMENTS_TXT_FILE: Path
+    ADDON_REQUIREMENTS_TXT_FILE: Path
+    DEV_REQUIREMENTS_TXT_FILE: Path
     MANIFEST_TOML_TXT_FILE: Path
     manifest_toml_file: Path
 
@@ -57,7 +62,7 @@ def get_args(defaults: ArgparseDefaults):
 
     parser.add_argument(
         "-v",
-        "--version",
+        "--blender_python_version",
         help="Version of Python interpreter within current Blender installation",
         type=str,
         default="3.11",
@@ -98,16 +103,21 @@ def prepare_context() -> Tuple[BuildContext, argparse.Namespace]:
 
     ADDON_SRC_DIR = args.addon_src_dir  # override defaults with new program args
 
+    version_tag = get_version_tag_from_git()
+
     timestamp: datetime.datetime = datetime.datetime.now()
     timestamp_str = f"# auto-generated in build: {timestamp} \n\n"
 
     wheels_dir: Path = Path(os.path.join(args.addon_src_dir, "wheels"))
     print(f"Wheels Directory Path: {wheels_dir}")
 
-    REQUIREMENTS_TXT_FILE: Path = Path(
+    ADDON_REQUIREMENTS_TXT_FILE: Path = Path(
         os.path.join(args.addon_src_dir, "requirements.txt")
     )
-    print(f"`requirements.txt` File Path: {REQUIREMENTS_TXT_FILE}")
+    print(f"Addon's `requirements.txt` File Path: {ADDON_REQUIREMENTS_TXT_FILE}")
+
+    DEV_REQUIREMENTS_TXT_FILE: Path = Path(os.path.join(repo_dir, "requirements.txt"))
+    print(f"Developer's `requirements.txt` File Path: {DEV_REQUIREMENTS_TXT_FILE}")
 
     MANIFEST_TOML_TXT_FILE: Path = Path(
         os.path.join(args.addon_src_dir, "blender_manifest.toml.txt")
@@ -124,12 +134,44 @@ def prepare_context() -> Tuple[BuildContext, argparse.Namespace]:
     )
 
 
-def install_pypi_wheels(requirements_txt_file):
+def get_version_tag_from_git() -> str:
+    """Retreive git tag to use as canonical version truth for `blender_manifest.toml`."""
+    try:
+        git_version_tag = subprocess.check_output(
+            ["git", "describe", "--tags", "--abbrev=0"],
+            text=True,
+        )
+        print(f"Retrieved git version tag: {git_version_tag}")
+
+        version_tag = git_version_tag.strip().removeprefix("v").split("-py")[0]
+        return version_tag
+    except subprocess.CalledProcessError:
+        raise RuntimeError("No git tags found.")
+
+
+def install_dev_pypi_packages(ctx: BuildContext):
     """install wheels for development"""
     print("Beginning install...")
     try:
         subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", "-r", str(requirements_txt_file)]
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "-r",
+                str(ctx.ADDON_REQUIREMENTS_TXT_FILE),
+            ]
+        )
+        subprocess.check_call(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "-r",
+                str(ctx.DEV_REQUIREMENTS_TXT_FILE),
+            ]
         )
 
     except subprocess.CalledProcessError as e:
@@ -140,7 +182,7 @@ def install_pypi_wheels(requirements_txt_file):
     print("Install complete.")
 
 
-def download_pypi_wheels(wheels_dir, requirements_txt_file, version, install):
+def download_pypi_wheels(ctx: BuildContext, version, install):
     """use `subprocess` to invoke `pip download ...` on the addon's `requirements.txt`."""
     try:
         subprocess.check_call(
@@ -150,9 +192,9 @@ def download_pypi_wheels(wheels_dir, requirements_txt_file, version, install):
                 "pip",
                 "download",
                 "-r",
-                str(requirements_txt_file),
+                str(ctx.ADDON_REQUIREMENTS_TXT_FILE),
                 "--dest",
-                str(wheels_dir),
+                str(ctx.wheels_dir),
                 "--only-binary=:all:",
                 f"--python-version={version}",
             ]
@@ -167,7 +209,7 @@ def download_pypi_wheels(wheels_dir, requirements_txt_file, version, install):
 
     if install:
         print()  # skip a line
-        install_pypi_wheels(requirements_txt_file)
+        install_dev_pypi_packages(ctx)
 
 
 def generate_blender_manifest_toml(ctx: BuildContext):
@@ -186,13 +228,40 @@ def generate_blender_manifest_toml(ctx: BuildContext):
         next(f)  # skip the comment on the first line
         template = f.read()
 
-    template = template.format(wheels_block=WHEELS_STR)
+    template = template.format(version_tag=ctx.version_tag, wheels_block=WHEELS_STR)
 
     with open(ctx.manifest_toml_file, "w", encoding="utf-8") as f:
         f.write(ctx.timestamp_str)
         f.write(template)
 
     print(f"`{ctx.manifest_toml_file}` successfully generated.")
+
+
+def package(ctx: BuildContext):
+    zip_path = ctx.ADDON_SRC_DIR.parent / f"{ctx.ADDON_SRC_DIR.name}.zip"
+
+    if zip_path.exists():
+        zip_path.unlink()
+
+    ignore_dirs = {"__pycache__"}
+    ignore_extensions = {".zip"}
+    ignore_filenames = {"blender_manifest.toml.txt"}
+
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_STORED) as zf:
+        for root, dirs, files in os.walk(ctx.ADDON_SRC_DIR):
+            # mutate dirs in-place to prevent descending
+            dirs[:] = [d for d in dirs if d not in ignore_dirs]
+
+            for file in files:
+                path = Path(root) / file
+
+                if file in ignore_filenames or path.suffix in ignore_extensions:
+                    continue
+
+                path_in_archive = path.relative_to(ctx.ADDON_SRC_DIR.parent)
+                zf.write(path, path_in_archive)
+
+    print(f"Addon packaged as `{zip_path}`")
 
 
 def clean(wheels_dir):
@@ -212,10 +281,11 @@ def main():
 
     for p in [
         ctx.ADDON_SRC_DIR,
-        ctx.REQUIREMENTS_TXT_FILE,
+        ctx.ADDON_REQUIREMENTS_TXT_FILE,
         ctx.MANIFEST_TOML_TXT_FILE,
+        *([ctx.DEV_REQUIREMENTS_TXT_FILE] if args.dev else []),
     ]:  # check that all required input resources are where they are supposed to be
-        if not os.path.exists(ctx.wheels_dir):
+        if not p.exists():
             raise RuntimeError(f"Expected {p!r} in file system, but was not found.")
 
     print()  # skip a line
@@ -224,15 +294,12 @@ def main():
         clean(ctx.wheels_dir)
         print()  # skip a line
 
-    download_pypi_wheels(
-        ctx.wheels_dir,
-        ctx.REQUIREMENTS_TXT_FILE,
-        args.version,
-        install=args.dev,
-    )
+    download_pypi_wheels(ctx, args.blender_python_version, install=args.dev)
     print()  # skip a line
 
     generate_blender_manifest_toml(ctx)
+
+    package(ctx)
 
 
 if __name__ == "__main__":
