560662ea79f36e3e1523dc722e8650a02473d448
mialana <aliu@amyliu.dev>
2026-01-20 17:24:19 -0500

    feat: start from dataclass transfer

--- mosplat_blender/infrastructure/schemas.py
+++ mosplat_blender/infrastructure/schemas.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-from typing import Dict, List
-from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, List, cast
+from dataclasses import dataclass, field, asdict
 from datetime import datetime
+import json
 
 
 @dataclass(frozen=True)
@@ -17,8 +19,12 @@ class PreprocessScriptApplication:
             application_time=datetime.now().timestamp(),
         )
 
+    @classmethod
+    def from_dict(cls, d: Dict) -> PreprocessScriptApplication:
+        return cls(**d)
 
-@dataclass(frozen=True)
+
+@dataclass
 class ProcessedFrameRange:
     start_frame: int
     end_frame: int
@@ -27,8 +33,13 @@ class ProcessedFrameRange:
     )
 
     @classmethod
-    def from_dict(cls, d: Dict):
-        return cls(**d)
+    def from_dict(cls, d: Dict) -> ProcessedFrameRange:
+        instance = cls(**d)
+        instance.applied_preprocess_scripts = [
+            PreprocessScriptApplication.from_dict(cast(Dict, a))
+            for a in instance.applied_preprocess_scripts
+        ]
+        return instance
 
 
 @dataclass
@@ -41,7 +52,7 @@ class MediaProcessStatus:
     file_size: int = -1
 
     @classmethod
-    def from_dict(cls, d: Dict):
+    def from_dict(cls, d: Dict) -> MediaProcessStatus:
         return cls(**d)
 
 
@@ -54,8 +65,36 @@ class MediaIOMetadata:
     processed_frame_ranges: List[ProcessedFrameRange] = field(default_factory=list)
 
     @classmethod
-    def from_dict(cls, d: Dict):
+    def from_dict(cls, d: Dict) -> MediaIOMetadata:
         instance = cls(**d)
         instance.media_process_statuses = [
-            MediaProcessStatus.from_dict(m) for m in instance.media_process_statuses
+            MediaProcessStatus.from_dict(
+                cast(Dict, m)
+            )  # note that the underlying structure of `m` is still a dict after `cls(**d)`
+            for m in instance.media_process_statuses
         ]
+        instance.processed_frame_ranges = [
+            ProcessedFrameRange.from_dict(cast(Dict, p))
+            for p in instance.processed_frame_ranges
+        ]
+        return instance
+
+    def to_JSON(self, dest_path: Path):
+        dest_path.parent.mkdir(parents=True, exist_ok=True)
+        as_dict = asdict(self)
+
+        with dest_path.open("w", encoding="utf-8") as f:
+            json.dump(as_dict, f, sort_keys=True, indent=4)
+
+    def from_JSON(self, src_path: Path) -> bool:
+        if src_path.exists():
+            try:
+                with src_path.open("r", encoding="utf-8") as f:
+                    data: Dict = json.load(f)
+
+                self.from_dict(data)
+                return True
+            except (TypeError, json.JSONDecodeError):
+                src_path.unlink()  # delete the corrupted JSON
+
+        return False
