ed237dd840c9874c3a33b58a3c530b8d558e043a
mialana <aliu@amyliu.dev>
2026-01-20 17:24:19 -0500

    feat: start from dataclass transfer

--- mosplat_blender/core/properties.py
+++ mosplat_blender/core/properties.py
@@ -15,9 +15,16 @@ from bpy.props import (
 )
 
 from pathlib import Path
+from typing import cast
 
 from ..infrastructure.mixins import MosplatBlPropertyAccessorMixin
 from ..infrastructure.constants import OperatorIDEnum
+from ..infrastructure.schemas import (
+    MediaIOMetadata,
+    MediaProcessStatus,
+    ProcessedFrameRange,
+    PreprocessScriptApplication,
+)
 
 
 def update_current_media_dir(props: Mosplat_PG_Global, _: Context):
@@ -28,6 +35,15 @@ class Mosplat_PG_PreprocessScriptApplication(PropertyGroup):
     script_path: StringProperty(name="Script Path", subtype="FILE_PATH")
     application_time: FloatProperty(name="Application Time", default=-1.0)
 
+    def to_dataclass(self) -> PreprocessScriptApplication:
+        return PreprocessScriptApplication(
+            script_path=self.script_path,
+            application_time=self.application_time,
+        )
+
+    def from_dataclass(self) -> None:
+        pass
+
 
 class Mosplat_PG_MediaProcessStatus(PropertyGroup):
     filepath: StringProperty(name="Filepath", subtype="FILE_PATH")
@@ -37,6 +53,16 @@ class Mosplat_PG_MediaProcessStatus(PropertyGroup):
     mod_time: FloatProperty(name="Modification Time", default=-1.0)
     file_size: IntProperty(name="File Size", default=-1)
 
+    def to_dataclass(self) -> MediaProcessStatus:
+        return MediaProcessStatus(
+            filepath=self.filepath,
+            frame_count=self.frame_count,
+            is_valid=self.is_valid,
+            message=self.message,
+            mod_time=self.mod_time,
+            file_size=self.file_size,
+        )
+
 
 class Mosplat_PG_ProcessedFrameRange(PropertyGroup):
     start_frame: IntProperty(name="Start Frame", default=0, min=0)
@@ -44,6 +70,16 @@ class Mosplat_PG_ProcessedFrameRange(PropertyGroup):
     applied_preprocess_scripts: CollectionProperty(
         name="Applied Preprocess Scripts", type=Mosplat_PG_PreprocessScriptApplication
     )
+
+    def to_dataclass(self) -> ProcessedFrameRange:
+        return ProcessedFrameRange(
+            start_frame=self.start_frame,
+            end_frame=self.end_frame,
+            applied_preprocess_scripts=[
+                cast(Mosplat_PG_PreprocessScriptApplication, a).to_dataclass()
+                for a in self.applied_preprocess_scripts
+            ],
+        )
 
 
 class Mosplat_PG_MediaIOMetadata(PropertyGroup):
@@ -74,6 +110,21 @@ class Mosplat_PG_MediaIOMetadata(PropertyGroup):
     processed_frame_ranges: CollectionProperty(
         name="Processed Frame Ranges", type=Mosplat_PG_ProcessedFrameRange
     )
+
+    def to_dataclass(self) -> MediaIOMetadata:
+        return MediaIOMetadata(
+            base_directory=self.base_directory,
+            do_media_durations_all_match=self.do_media_durations_all_match,
+            collective_media_frame_count=self.collective_media_frame_count,
+            media_process_statuses=[
+                cast(Mosplat_PG_MediaProcessStatus, m).to_dataclass()
+                for m in self.media_process_statuses
+            ],
+            processed_frame_ranges=[
+                cast(Mosplat_PG_ProcessedFrameRange, p).to_dataclass()
+                for p in self.processed_frame_ranges
+            ],
+        )
 
 
 class Mosplat_PG_Global(PropertyGroup, MosplatBlPropertyAccessorMixin):
